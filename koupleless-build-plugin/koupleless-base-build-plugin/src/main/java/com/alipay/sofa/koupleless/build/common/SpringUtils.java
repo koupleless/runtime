d06fb53493cef46014dfa5c4cc773738d41f8989
CodeNoobKing <67936979+CodeNoobKing@users.noreply.github.com>
2024-03-21 15:42:56 +0800

    <fix>

--- koupleless-build-plugin/koupleless-base-build-plugin/src/main/java/com/alipay/sofa/koupleless/build/common/SpringUtils.java
+++ koupleless-build-plugin/koupleless-base-build-plugin/src/main/java/com/alipay/sofa/koupleless/build/common/SpringUtils.java
@@ -18,11 +18,8 @@ package com.alipay.sofa.koupleless.build.common;
 
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.io.support.SpringFactoriesLoader;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.*;
 
 /**
