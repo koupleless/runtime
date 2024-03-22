4c12e9acc027407f35255bf8d7785e77fb5359d0
gaosaroma <gaosaroma@gmail.com>
2024-03-22 10:27:13 +0800

    add unit test

--- arklet-springboot-starter/src/test/java/com/alipay/sofa/koupleless/arklet/springboot/starter/health/ArkBizAggregateHealthIndicatorTest.java
+++ arklet-springboot-starter/src/test/java/com/alipay/sofa/koupleless/arklet/springboot/starter/health/ArkBizAggregateHealthIndicatorTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.koupleless.arklet.springboot.starter.health;
 
 import com.alipay.sofa.ark.api.ArkClient;
@@ -29,6 +45,7 @@ import static org.mockito.Mockito.mockStatic;
  */
 public class ArkBizAggregateHealthIndicatorTest {
     private ArkBizAggregateHealthIndicator indicator = new ArkBizAggregateHealthIndicator();
+
     @Test
     public void test() {
         try (MockedStatic<BizRuntimeContextRegistry> registry = mockStatic(BizRuntimeContextRegistry.class)) {
@@ -69,7 +86,7 @@ public class ArkBizAggregateHealthIndicatorTest {
         }
     }
 
-    private Biz mockMastertBiz(){
+    private Biz mockMastertBiz() {
         ClassLoader masterBizClassLoader = mock(ClassLoader.class);
         BizModel masterBiz = new BizModel();
         masterBiz.setClassLoader(masterBizClassLoader);
@@ -79,7 +96,7 @@ public class ArkBizAggregateHealthIndicatorTest {
         return masterBiz;
     }
 
-    private Biz mockBiz1(){
+    private Biz mockBiz1() {
         ClassLoader bizClassLoader = mock(ClassLoader.class);
         BizModel biz = new BizModel();
         biz.setClassLoader(bizClassLoader);
