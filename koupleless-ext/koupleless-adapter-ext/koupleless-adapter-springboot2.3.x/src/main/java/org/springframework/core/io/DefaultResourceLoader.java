ad8d0f8093bc2da2d720e3a550874e66e3218c5f
jiyunfei <jiyunfei@come-future.com>
2024-04-09 16:51:51 +0800

    format

--- koupleless-ext/koupleless-adapter-ext/koupleless-adapter-springboot2.3.x/src/main/java/org/springframework/core/io/DefaultResourceLoader.java
+++ koupleless-ext/koupleless-adapter-ext/koupleless-adapter-springboot2.3.x/src/main/java/org/springframework/core/io/DefaultResourceLoader.java
@@ -1,11 +1,12 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.core.io;
 
 import java.net.MalformedURLException;
@@ -48,166 +48,163 @@ import org.springframework.util.StringUtils;
  */
 public class DefaultResourceLoader implements ResourceLoader {
 
-	@Nullable
-	private ClassLoader classLoader;
+    @Nullable
+    private ClassLoader                           classLoader;
 
-	private final Set<ProtocolResolver> protocolResolvers = new LinkedHashSet<>(4);
+    private final Set<ProtocolResolver>           protocolResolvers = new LinkedHashSet<>(4);
 
-	private final Map<Class<?>, Map<Resource, ?>> resourceCaches = new ConcurrentHashMap<>(4);
+    private final Map<Class<?>, Map<Resource, ?>> resourceCaches    = new ConcurrentHashMap<>(4);
 
+    /**
+     * Create a new DefaultResourceLoader.
+     * <p>ClassLoader access will happen using the thread context class loader
+     * at the time of actual resource access (since 5.3). For more control, pass
+     * a specific ClassLoader to {@link #DefaultResourceLoader(ClassLoader)}.
+     * @see Thread#getContextClassLoader()
+     */
+    public DefaultResourceLoader() {
+    }
 
-	/**
-	 * Create a new DefaultResourceLoader.
-	 * <p>ClassLoader access will happen using the thread context class loader
-	 * at the time of actual resource access (since 5.3). For more control, pass
-	 * a specific ClassLoader to {@link #DefaultResourceLoader(ClassLoader)}.
-	 * @see Thread#getContextClassLoader()
-	 */
-	public DefaultResourceLoader() {
-	}
+    /**
+     * Create a new DefaultResourceLoader.
+     * @param classLoader the ClassLoader to load class path resources with, or {@code null}
+     * for using the thread context class loader at the time of actual resource access
+     */
+    public DefaultResourceLoader(@Nullable ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
-	/**
-	 * Create a new DefaultResourceLoader.
-	 * @param classLoader the ClassLoader to load class path resources with, or {@code null}
-	 * for using the thread context class loader at the time of actual resource access
-	 */
-	public DefaultResourceLoader(@Nullable ClassLoader classLoader) {
-		this.classLoader = classLoader;
-	}
+    /**
+     * Specify the ClassLoader to load class path resources with, or {@code null}
+     * for using the thread context class loader at the time of actual resource access.
+     * <p>The default is that ClassLoader access will happen using the thread context
+     * class loader at the time of actual resource access (since 5.3).
+     */
+    public void setClassLoader(@Nullable ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
+    /**
+     * Return the ClassLoader to load class path resources with.
+     * <p>Will get passed to ClassPathResource's constructor for all
+     * ClassPathResource objects created by this resource loader.
+     * @see ClassPathResource
+     */
+    @Override
+    @Nullable
+    public ClassLoader getClassLoader() {
+        return (this.classLoader != null ? this.classLoader : ClassUtils.getDefaultClassLoader());
+    }
 
-	/**
-	 * Specify the ClassLoader to load class path resources with, or {@code null}
-	 * for using the thread context class loader at the time of actual resource access.
-	 * <p>The default is that ClassLoader access will happen using the thread context
-	 * class loader at the time of actual resource access (since 5.3).
-	 */
-	public void setClassLoader(@Nullable ClassLoader classLoader) {
-		this.classLoader = classLoader;
-	}
+    /**
+     * Register the given resolver with this resource loader, allowing for
+     * additional protocols to be handled.
+     * <p>Any such resolver will be invoked ahead of this loader's standard
+     * resolution rules. It may therefore also override any default rules.
+     * @since 4.3
+     * @see #getProtocolResolvers()
+     */
+    public void addProtocolResolver(ProtocolResolver resolver) {
+        Assert.notNull(resolver, "ProtocolResolver must not be null");
+        this.protocolResolvers.add(resolver);
+    }
 
-	/**
-	 * Return the ClassLoader to load class path resources with.
-	 * <p>Will get passed to ClassPathResource's constructor for all
-	 * ClassPathResource objects created by this resource loader.
-	 * @see ClassPathResource
-	 */
-	@Override
-	@Nullable
-	public ClassLoader getClassLoader() {
-		return (this.classLoader != null ? this.classLoader : ClassUtils.getDefaultClassLoader());
-	}
+    /**
+     * Return the collection of currently registered protocol resolvers,
+     * allowing for introspection as well as modification.
+     * @since 4.3
+     * @see #addProtocolResolver(ProtocolResolver)
+     */
+    public Collection<ProtocolResolver> getProtocolResolvers() {
+        return this.protocolResolvers;
+    }
 
-	/**
-	 * Register the given resolver with this resource loader, allowing for
-	 * additional protocols to be handled.
-	 * <p>Any such resolver will be invoked ahead of this loader's standard
-	 * resolution rules. It may therefore also override any default rules.
-	 * @since 4.3
-	 * @see #getProtocolResolvers()
-	 */
-	public void addProtocolResolver(ProtocolResolver resolver) {
-		Assert.notNull(resolver, "ProtocolResolver must not be null");
-		this.protocolResolvers.add(resolver);
-	}
+    /**
+     * Obtain a cache for the given value type, keyed by {@link Resource}.
+     * @param valueType the value type, e.g. an ASM {@code MetadataReader}
+     * @return the cache {@link Map}, shared at the {@code ResourceLoader} level
+     * @since 5.0
+     */
+    @SuppressWarnings("unchecked")
+    public <T> Map<Resource, T> getResourceCache(Class<T> valueType) {
+        return (Map<Resource, T>) this.resourceCaches.computeIfAbsent(valueType,
+            key -> new ConcurrentHashMap<>());
+    }
 
-	/**
-	 * Return the collection of currently registered protocol resolvers,
-	 * allowing for introspection as well as modification.
-	 * @since 4.3
-	 * @see #addProtocolResolver(ProtocolResolver)
-	 */
-	public Collection<ProtocolResolver> getProtocolResolvers() {
-		return this.protocolResolvers;
-	}
+    /**
+     * Clear all resource caches in this resource loader.
+     * @since 5.0
+     * @see #getResourceCache
+     */
+    public void clearResourceCaches() {
+        this.resourceCaches.clear();
+    }
 
-	/**
-	 * Obtain a cache for the given value type, keyed by {@link Resource}.
-	 * @param valueType the value type, e.g. an ASM {@code MetadataReader}
-	 * @return the cache {@link Map}, shared at the {@code ResourceLoader} level
-	 * @since 5.0
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> Map<Resource, T> getResourceCache(Class<T> valueType) {
-		return (Map<Resource, T>) this.resourceCaches.computeIfAbsent(valueType, key -> new ConcurrentHashMap<>());
-	}
+    @Override
+    public Resource getResource(String location) {
+        Assert.notNull(location, "Location must not be null");
 
-	/**
-	 * Clear all resource caches in this resource loader.
-	 * @since 5.0
-	 * @see #getResourceCache
-	 */
-	public void clearResourceCaches() {
-		this.resourceCaches.clear();
-	}
+        for (ProtocolResolver protocolResolver : getProtocolResolvers()) {
+            Resource resource = protocolResolver.resolve(location, this);
+            if (resource != null) {
+                return resource;
+            }
+        }
 
+        if (location.startsWith("/")) {
+            return getResourceByPath(location);
+        } else if (location.startsWith(CLASSPATH_URL_PREFIX)) {
+            return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()),
+                getClassLoader());
+        } else {
+            try {
+                // Try to parse the location as a URL...
+                URL url = new URL(location);
+                return (ResourceUtils.isFileURL(url) ? new FileUrlResource(url)
+                    : new UrlResource(url));
+            } catch (MalformedURLException ex) {
+                // No URL -> resolve as resource path.
+                return getResourceByPath(location);
+            }
+        }
+    }
 
-	@Override
-	public Resource getResource(String location) {
-		Assert.notNull(location, "Location must not be null");
+    /**
+     * Return a Resource handle for the resource at the given path.
+     * <p>The default implementation supports class path locations. This should
+     * be appropriate for standalone implementations but can be overridden,
+     * e.g. for implementations targeted at a Servlet container.
+     * @param path the path to the resource
+     * @return the corresponding Resource handle
+     * @see ClassPathResource
+     * @see org.springframework.context.support.FileSystemXmlApplicationContext#getResourceByPath
+     * @see org.springframework.web.context.support.XmlWebApplicationContext#getResourceByPath
+     */
+    protected Resource getResourceByPath(String path) {
+        return new ClassPathContextResource(path, getClassLoader());
+    }
 
-		for (ProtocolResolver protocolResolver : getProtocolResolvers()) {
-			Resource resource = protocolResolver.resolve(location, this);
-			if (resource != null) {
-				return resource;
-			}
-		}
+    /**
+     * ClassPathResource that explicitly expresses a context-relative path
+     * through implementing the ContextResource interface.
+     */
+    protected static class ClassPathContextResource extends ClassPathResource
+                                                    implements ContextResource {
 
-		if (location.startsWith("/")) {
-			return getResourceByPath(location);
-		}
-		else if (location.startsWith(CLASSPATH_URL_PREFIX)) {
-			return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()), getClassLoader());
-		}
-		else {
-			try {
-				// Try to parse the location as a URL...
-				URL url = new URL(location);
-				return (ResourceUtils.isFileURL(url) ? new FileUrlResource(url) : new UrlResource(url));
-			}
-			catch (MalformedURLException ex) {
-				// No URL -> resolve as resource path.
-				return getResourceByPath(location);
-			}
-		}
-	}
+        public ClassPathContextResource(String path, @Nullable ClassLoader classLoader) {
+            super(path, classLoader);
+        }
 
-	/**
-	 * Return a Resource handle for the resource at the given path.
-	 * <p>The default implementation supports class path locations. This should
-	 * be appropriate for standalone implementations but can be overridden,
-	 * e.g. for implementations targeted at a Servlet container.
-	 * @param path the path to the resource
-	 * @return the corresponding Resource handle
-	 * @see ClassPathResource
-	 * @see org.springframework.context.support.FileSystemXmlApplicationContext#getResourceByPath
-	 * @see org.springframework.web.context.support.XmlWebApplicationContext#getResourceByPath
-	 */
-	protected Resource getResourceByPath(String path) {
-		return new ClassPathContextResource(path, getClassLoader());
-	}
+        @Override
+        public String getPathWithinContext() {
+            return getPath();
+        }
 
-
-	/**
-	 * ClassPathResource that explicitly expresses a context-relative path
-	 * through implementing the ContextResource interface.
-	 */
-	protected static class ClassPathContextResource extends ClassPathResource implements ContextResource {
-
-		public ClassPathContextResource(String path, @Nullable ClassLoader classLoader) {
-			super(path, classLoader);
-		}
-
-		@Override
-		public String getPathWithinContext() {
-			return getPath();
-		}
-
-		@Override
-		public Resource createRelative(String relativePath) {
-			String pathToUse = StringUtils.applyRelativePath(getPath(), relativePath);
-			return new ClassPathContextResource(pathToUse, getClassLoader());
-		}
-	}
+        @Override
+        public Resource createRelative(String relativePath) {
+            String pathToUse = StringUtils.applyRelativePath(getPath(), relativePath);
+            return new ClassPathContextResource(pathToUse, getClassLoader());
+        }
+    }
 
 }
