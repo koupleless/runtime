dfbe37e207e13ba15c93e08a63d6da844d736ffe
gaosaroma <gaosaroma@gmail.com>
2024-05-14 00:28:45 +0800

    增加调度线程池与classloader绑定
    (cherry picked from commit d072fecfa03c685ef20bd74dd76a747832e01e4a)

--- koupleless-common/src/test/java/com/alipay/sofa/koupleless/common/util/KouplelessThreadPoolExecutorTest.java
+++ koupleless-common/src/test/java/com/alipay/sofa/koupleless/common/util/KouplelessThreadPoolExecutorTest.java
@@ -14,10 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alipay.sofa.koupleless.common;
+package com.alipay.sofa.koupleless.common.util;
 
 import com.alipay.sofa.ark.common.util.ClassLoaderUtils;
-import com.alipay.sofa.koupleless.common.util.KouplelessThreadPoolExecutor;
+import org.junit.After;
 import org.junit.Test;
 
 import java.net.URLClassLoader;
@@ -32,26 +32,27 @@ import static org.junit.Assert.assertEquals;
  * @version $Id: KouplelessThreadPoolExecutorTest.java, v 0.1 2024年05月10日 12:04 立蓬 Exp $
  */
 public class KouplelessThreadPoolExecutorTest {
-    @Test
-    public void testSubmit() {
-        KouplelessThreadPoolExecutor executor = new KouplelessThreadPoolExecutor(10, 10, 60L,
-            TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
 
+    private URLClassLoader classLoader = mockClassLoader();
+    private KouplelessThreadPoolExecutor executor = new KouplelessThreadPoolExecutor(10, 10, 60L,
+            TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+    @Test
+    public void test() {
         ClassLoader currentCl = Thread.currentThread().getContextClassLoader();
         try {
-            URLClassLoader mockClassLoader = mockClassLoader();
-            Thread.currentThread().setContextClassLoader(mockClassLoader);
-            executor.submit(() -> {
-                assertEquals(mockClassLoader, Thread.currentThread().getContextClassLoader());
-            });
-            executor.submit(new Callable<String>() {
-                @Override
-                public String call() throws Exception {
-                    assertEquals(mockClassLoader, Thread.currentThread().getContextClassLoader());
-                    return "mock";
-                }
-            });
-            executor.shutdown();
+
+            Runnable runnable = () -> assertEquals(classLoader, Thread.currentThread().getContextClassLoader());
+
+            Callable<String> callable = () -> {
+                assertEquals(classLoader, Thread.currentThread().getContextClassLoader());
+                return "mock";
+            };
+
+            Thread.currentThread().setContextClassLoader(classLoader);
+            executor.submit(runnable);
+            executor.submit(callable);
+            executor.submit(runnable,"mock");
+
         } finally {
             Thread.currentThread().setContextClassLoader(currentCl);
         }
@@ -59,5 +60,10 @@ public class KouplelessThreadPoolExecutorTest {
 
     private URLClassLoader mockClassLoader() {
         return new URLClassLoader(ClassLoaderUtils.getURLs(this.getClass().getClassLoader()));
+    }
+
+    @After
+    public void after(){
+        executor.shutdown();
     }
 }
