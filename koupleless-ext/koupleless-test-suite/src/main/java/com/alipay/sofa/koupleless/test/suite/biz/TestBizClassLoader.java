8680e08b3b07c2ad07c6c30dae8e09e311743e12
CodeNoobKing <67936979+CodeNoobKing@users.noreply.github.com>
2024-05-21 14:08:33 +0800

    log

--- koupleless-ext/koupleless-test-suite/src/main/java/com/alipay/sofa/koupleless/test/suite/biz/TestBizClassLoader.java
+++ koupleless-ext/koupleless-test-suite/src/main/java/com/alipay/sofa/koupleless/test/suite/biz/TestBizClassLoader.java
@@ -56,7 +56,7 @@ public class TestBizClassLoader extends BizClassLoader {
         bizClassLoaderHookField.setAccessible(false);
 
         Field bizClassLoaderIsHookLoadedField = BizClassLoader.class
-            .getDeclaredField("isHookLoaded");
+                .getDeclaredField("isHookLoaded");
         bizClassLoaderIsHookLoadedField.setAccessible(true);
         bizClassLoaderIsHookLoadedField.set(this, new AtomicBoolean(true));
         bizClassLoaderIsHookLoadedField.setAccessible(false);
@@ -82,7 +82,8 @@ public class TestBizClassLoader extends BizClassLoader {
         this.resolveByClassLoaderPatterns.addAll(CollectionUtils.emptyIfNull(includeClassNames));
         this.includedArtifactIds.addAll(CollectionUtils.emptyIfNull(includeArtifactIds));
 
-        ArkletLoggerFactory.getDefaultLogger().info("{}, BaseUrlsForBiz", baseClassLoader.getURLs());
+        ArkletLoggerFactory.getDefaultLogger().info(
+                "{}, {} ", baseClassLoader.getURLs(), "BaseUrlsForBiz");
         ArkletLoggerFactory.getDefaultLogger().info("{}, IncludeClassNames", includeClassNames);
         ArkletLoggerFactory.getDefaultLogger().info("{}, IncludeClassPatterns", includeClassNames);
         ArkletLoggerFactory.getDefaultLogger().info("{}, IncludeArtifactIds", includeArtifactIds);
@@ -129,8 +130,8 @@ public class TestBizClassLoader extends BizClassLoader {
         Class<?> clz = super.loadClassInternal(name, resolve);
 
         String codeSourceLocation = Optional.ofNullable(clz.getProtectionDomain())
-            .map(ProtectionDomain::getCodeSource).map(CodeSource::getLocation).map(URL::toString)
-            .orElse("");
+                .map(ProtectionDomain::getCodeSource).map(CodeSource::getLocation).map(URL::toString)
+                .orElse("");
 
         for (String includedArtifactId : includedArtifactIds) {
             // should be load by test class loader
