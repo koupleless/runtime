c7a063a1c9cc6a9c90ca91ddd68b8cc9314b8d1c
gaosaroma <gaosaroma@gmail.com>
2024-07-02 15:52:01 +0800

    add metrics monitor
    (cherry picked from commit 97525f44003d8c1b061910b9091e6d4499e735b5)

--- arklet-core/src/main/java/com/alipay/sofa/koupleless/arklet/core/monitor/MetricsMonitor.java
+++ arklet-core/src/main/java/com/alipay/sofa/koupleless/arklet/core/monitor/MetricsMonitor.java
@@ -16,12 +16,20 @@
  */
 package com.alipay.sofa.koupleless.arklet.core.monitor;
 
-import com.alipay.sofa.koupleless.arklet.core.health.model.ClientMetrics;
+import com.alipay.sofa.koupleless.arklet.core.monitor.model.ClientMetrics;
+import com.alipay.sofa.koupleless.arklet.core.monitor.model.GarbageCollectorName;
 import com.alipay.sofa.koupleless.arklet.core.util.DateUtils;
 
+import java.lang.management.ClassLoadingMXBean;
+import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryUsage;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.management.ThreadMXBean;
+import java.util.List;
+
+import static com.alipay.sofa.koupleless.arklet.core.health.model.Constants.BYTE_TO_MB;
 
 /**
  * @author lianglipeng.llp@alibaba-inc.com
@@ -31,7 +39,10 @@ public class MetricsMonitor {
 
     public static ClientMetrics captureMetrics() {
         return ClientMetrics.builder().timestamp(DateUtils.getCurrentTime())
-            .clientMetaspaceMetrics(reportMetaspace()).build();
+            .metaspaceMetrics(reportMetaspace()).GCMetrics(reportGC())
+            .metaspaceMetrics(reportMetaspace()).noHeapMetrics(reportNoheap())
+            .heapMetrics(reportHeap()).systemMetrics(reportSystem()).threadMetrics(reportThread())
+            .classLoadingMetrics(reportClassLoading()).build();
     }
 
     private static ClientMetrics.ClientMemoryMetrics reportMetaspace() {
@@ -45,11 +56,77 @@ public class MetricsMonitor {
         return null;
     }
 
+    private static ClientMetrics.ClientMemoryMetrics reportHeap() {
+        MemoryUsage usage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+        return new ClientMetrics.ClientMemoryMetrics(usage.getInit(), usage.getUsed(),
+            usage.getCommitted(), usage.getMax());
+    }
+
+    private static ClientMetrics.ClientMemoryMetrics reportNoheap() {
+        MemoryUsage usage = ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage();
+        return new ClientMetrics.ClientMemoryMetrics(usage.getInit(), usage.getUsed(),
+            usage.getCommitted(), usage.getMax());
+    }
+
+    private static ClientMetrics.ClientSystemMetrics reportSystem() {
+        OperatingSystemMXBean origin = ManagementFactory.getOperatingSystemMXBean();
+        double systemLoadAverage = origin.getSystemLoadAverage();
+        double processCpuLoad = 0;
+        double systemCpuLoad = 0;
+        long freePhysicalMemorySize = 0L;
+        long totalPhysicalMemorySize = 0L;
+        if (origin instanceof com.sun.management.OperatingSystemMXBean) {
+            com.sun.management.OperatingSystemMXBean operatingSystemMXBean = (com.sun.management.OperatingSystemMXBean) origin;
+            processCpuLoad = operatingSystemMXBean.getProcessCpuLoad();
+            systemCpuLoad = operatingSystemMXBean.getSystemCpuLoad();
+            freePhysicalMemorySize = operatingSystemMXBean.getFreePhysicalMemorySize() / BYTE_TO_MB;
+            totalPhysicalMemorySize = operatingSystemMXBean.getTotalPhysicalMemorySize()
+                                      / BYTE_TO_MB;
+        }
+        return new ClientMetrics.ClientSystemMetrics(systemLoadAverage, processCpuLoad,
+            systemCpuLoad, freePhysicalMemorySize, totalPhysicalMemorySize);
+    }
+
+    private static ClientMetrics.ClientGCMetrics reportGC() {
+        long FGCCount = 0, FGCTime = 0, YGCCount = 0, YGCTime = 0;
+        List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gc : gcs) {
+            if (GarbageCollectorName.belongToFgc(gc.getName())) {
+                FGCCount += gc.getCollectionCount();
+                FGCTime += gc.getCollectionTime();
+            }
+            if (GarbageCollectorName.belongToYgc(gc.getName())) {
+                YGCCount += gc.getCollectionCount();
+                YGCTime += gc.getCollectionTime();
+            }
+        }
+        return new ClientMetrics.ClientGCMetrics(FGCCount, FGCTime, YGCCount, YGCTime);
+    }
+
+    private static ClientMetrics.ClientThreadMetrics reportThread() {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        int threadCount = threadMXBean.getThreadCount();
+        int daemonThreadCount = threadMXBean.getDaemonThreadCount();
+        int peakThreadCount = threadMXBean.getPeakThreadCount();
+        long totalStartedThreadCount = threadMXBean.getTotalStartedThreadCount();
+        return new ClientMetrics.ClientThreadMetrics(threadCount, daemonThreadCount,
+            peakThreadCount, totalStartedThreadCount);
+    }
+
+    private static ClientMetrics.ClientClassLoadingMetrics reportClassLoading() {
+        ClassLoadingMXBean classLoadingMXBean = ManagementFactory.getClassLoadingMXBean();
+        long totalLoadedClassCount = classLoadingMXBean.getTotalLoadedClassCount();
+        int loadedClassCount = classLoadingMXBean.getLoadedClassCount();
+        long unloadedClassCount = classLoadingMXBean.getUnloadedClassCount();
+        return new ClientMetrics.ClientClassLoadingMetrics(totalLoadedClassCount, loadedClassCount,
+            unloadedClassCount);
+    }
+
     public static boolean validateMetaspace(long metaspaceThreshold, ClientMetrics metrics) {
         if (metrics == null) {
             return true;
         }
-        ClientMetrics.ClientMemoryMetrics metaspaceReport = metrics.getClientMetaspaceMetrics();
+        ClientMetrics.ClientMemoryMetrics metaspaceReport = metrics.getMetaspaceMetrics();
         long committed = metaspaceReport.getCommitted();
         long max = metaspaceReport.getMax();
         return (max == -1) || ((double) committed / max) * 100 <= metaspaceThreshold;
