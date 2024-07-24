dddb0159e62382636f4989217768a7c7c1db9d9c
Lipeng <44571204+gaosaroma@users.noreply.github.com>
2024-07-24 18:03:54 +0800

    新版模块瘦身实现 (#136)
    * add basePackageDependencyMojo
    
    * add basePackageFacadeMojo
    
    * upgrade to 1.2.4-SNAPSHOT

---
+++ koupleless-ext/koupleless-build-plugin/koupleless-base-build-plugin/src/main/java/com/alipay/sofa/koupleless/base/build/plugin/KouplelessBasePackageDependencyMojo.java
@@ -0,0 +1,203 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.koupleless.base.build.plugin;
+
+import com.alipay.sofa.koupleless.base.build.plugin.model.ArtifactItem;
+import com.alipay.sofa.koupleless.base.build.plugin.utils.MavenUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
+import org.apache.maven.model.License;
+import org.apache.maven.model.Model;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.alipay.sofa.koupleless.base.build.plugin.common.FileUtils.createNewDirectory;
+import static com.alipay.sofa.koupleless.base.build.plugin.utils.CollectionUtils.nonNull;
+import static com.alipay.sofa.koupleless.base.build.plugin.utils.MavenUtils.getAllBundleArtifact;
+import static com.alipay.sofa.koupleless.base.build.plugin.utils.MavenUtils.getDependencyArtifacts;
+import static com.alipay.sofa.koupleless.base.build.plugin.utils.MavenUtils.getPomFileOfBundle;
+import static com.alipay.sofa.koupleless.base.build.plugin.utils.MavenUtils.getRootProject;
+import static com.alipay.sofa.koupleless.base.build.plugin.utils.MavenUtils.invoke;
+
+/**
+ * @author lianglipeng.llp@alibaba-inc.com
+ * @version $Id: KouplelessBasePackageDependencyMojo.java, v 0.1 2024年07月15日 11:59 立蓬 Exp $
+ */
+@Mojo(name = "packageDependency", defaultPhase = LifecyclePhase.PACKAGE, requiresProject = true, threadSafe = true, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+public class KouplelessBasePackageDependencyMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject mavenProject;
+
+    @Component
+    private MavenSession mavenSession;
+
+    @Parameter(defaultValue = "${project.basedir}", required = true)
+    private File         baseDir;
+
+    @Parameter(defaultValue = "${project.version}")
+    private String       dependencyVersion;
+
+    @Parameter(defaultValue = "dependencyArtifact")
+    private String       dependencyArtifactId;
+
+    @Parameter(defaultValue = "${project.groupId}", required = true)
+    private String       dependencyGroupId;
+
+    private File         dependencyRootDir;
+
+    @Parameter(defaultValue = "true")
+    private String       cleanAfterPackageDependencies;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            createBaseDependency();
+        } catch (Throwable t) {
+            getLog().error(t);
+            throw new RuntimeException(t);
+        } finally {
+            clearDependencyRootDir();
+        }
+    }
+
+    private void createBaseDependency() throws Exception {
+        createDependencyRootDir();
+        integrateBaseDependencies();
+        installBaseDependencies();
+        moveToOutputs();
+    }
+
+    private void moveToOutputs() throws IOException {
+        File outputsDir = new File(baseDir, "outputs");
+        createNewDirectory(outputsDir);
+
+        File newPomFile = new File(outputsDir, "pom.xml");
+        Files.copy(getPomFileOfBundle(dependencyRootDir).toPath(), newPomFile.toPath(),
+            StandardCopyOption.REPLACE_EXISTING);
+        getLog().info("copy pom.xml to " + newPomFile.getAbsolutePath() + " success.");
+    }
+
+    protected void installBaseDependencies() throws MojoExecutionException,
+                                             MavenInvocationException {
+        try {
+            InvocationResult result = invoke(mavenSession, "install",
+                getPomFileOfBundle(dependencyRootDir));
+            if (result.getExitCode() != 0) {
+                throw new MojoExecutionException("execute mvn install failed for base dependencies",
+                    result.getExecutionException());
+            }
+        } catch (Exception e) {
+            getLog().error("execute mvn install failed for base dependencies", e);
+            throw e;
+        }
+        getLog().info("package base dependencies success.");
+    }
+
+    private void createDependencyRootDir() throws IOException {
+        //0. 创建一个空maven工程
+        File rootDir = new File(baseDir, dependencyArtifactId);
+        createNewDirectory(rootDir);
+
+        File facadePom = new File(rootDir, "pom.xml");
+        if (!facadePom.exists()) {
+            facadePom.createNewFile();
+        }
+        getLog().info("create base dependency directory success." + rootDir.getAbsolutePath());
+        dependencyRootDir = rootDir;
+    }
+
+    private void integrateBaseDependencies() throws MojoExecutionException, IOException {
+        Model pom = new Model();
+        // 设置 xml 头
+        pom.setModelEncoding("UTF-8");
+        pom.setVersion("1.0");
+        pom.setModelVersion("4.0.0");
+
+        // 设置 parent
+        MavenProject rootProject = getRootProject(mavenProject);
+        pom.setParent(rootProject.getOriginalModel().getParent());
+
+        // 设置 groupId, artifactId, version
+        pom.setGroupId(dependencyGroupId);
+        pom.setArtifactId(dependencyArtifactId);
+        pom.setVersion(dependencyVersion);
+        pom.setPackaging("pom");
+
+        // 配置 license
+        License license = new License();
+        license.setName("The Apache License, Version 2.0");
+        license.setUrl("http://www.apache.org/licenses/LICENSE-2.0.txt");
+        pom.setLicenses(Collections.singletonList(license));
+
+        // 配置 properties
+        pom.setProperties(this.mavenProject.getProperties());
+
+        // 配置 dependencyManagement
+        Set<ArtifactItem> baseModuleArtifacts = getAllBundleArtifact(this.mavenProject);
+        getLog().info("find maven module of base: " + baseModuleArtifacts);
+        Set<Artifact> dependencyArtifacts = getDependencyArtifacts(this.mavenProject);
+        DependencyManagement dependencyManagement = new DependencyManagement();
+        List<Dependency> dependencies = nonNull(dependencyArtifacts).stream()
+            // 过滤出不属于项目的依赖
+            .filter(d -> baseModuleArtifacts.stream().noneMatch(
+                baseModule -> Objects.equals(baseModule.getGroupId(), d.getGroupId())
+                              && Objects.equals(baseModule.getArtifactId(), d.getArtifactId())))
+            // 过滤出 scope 不是 test 的依赖
+            .filter(d -> !"test".equals(d.getScope())).map(MavenUtils::createDependency)
+            .collect(Collectors.toList());
+        dependencyManagement.setDependencies(dependencies);
+        pom.setDependencyManagement(dependencyManagement);
+
+        // 配置 build
+        Model baseDependencyPomTemplate = MavenUtils.buildPomModel(this.getClass().getClassLoader()
+            .getResourceAsStream("base-dependency-pom-template.xml"));
+        Build build = baseDependencyPomTemplate.getBuild().clone();
+        pom.setBuild(build);
+
+        MavenUtils.writePomModel(getPomFileOfBundle(dependencyRootDir), pom);
+    }
+
+    private void clearDependencyRootDir() {
+        if (Boolean.parseBoolean(cleanAfterPackageDependencies) && dependencyRootDir != null) {
+            FileUtils.deleteQuietly(dependencyRootDir);
+        }
+    }
+}
---
+++ koupleless-ext/koupleless-build-plugin/koupleless-base-build-plugin/src/main/java/com/alipay/sofa/koupleless/base/build/plugin/utils/MavenUtils.java
@@ -0,0 +1,209 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.koupleless.base.build.plugin.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.apache.maven.plugin.MojoExecutionException;
+import com.alipay.sofa.koupleless.base.build.plugin.model.ArtifactItem;
+import org.apache.maven.plugins.dependency.resolvers.ListMojo;
+import org.apache.maven.plugins.dependency.utils.DependencyStatusSets;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @author lianglipeng.llp@alibaba-inc.com
+ * @version $Id: MavenUtils.java, v 0.1 2024年07月15日 13:58 立蓬 Exp $
+ */
+public class MavenUtils {
+
+    private static MavenXpp3Reader reader = new MavenXpp3Reader();
+    private static MavenXpp3Writer writer = new MavenXpp3Writer();
+
+    public static MavenProject getRootProject(MavenProject project) {
+        if (project == null) {
+            return null;
+        }
+        MavenProject parent = project;
+        while (parent.hasParent() && parent.getParent().getBasedir() != null) {
+            parent = parent.getParent();
+        }
+        return parent;
+    }
+
+    public static Set<ArtifactItem> getAllBundleArtifact(MavenProject project) {
+        File basedir = getRootProject(project).getBasedir();
+        return getBundlesArtifact(basedir.getAbsolutePath());
+    }
+
+    public static Dependency createDependency(Artifact artifact) {
+        Dependency d = new Dependency();
+        d.setArtifactId(artifact.getArtifactId());
+        d.setVersion(artifact.getVersion());
+        d.setGroupId(artifact.getGroupId());
+        if (!"jar".equals(artifact.getType())) {
+            d.setType(artifact.getType());
+        }
+        if (artifact.hasClassifier()) {
+            d.setClassifier(artifact.getClassifier());
+        }
+        return d;
+    }
+
+    private static Set<ArtifactItem> getBundlesArtifact(String bundlePath) {
+        Set<ArtifactItem> results = new HashSet<>();
+        Model pom = buildPomModel(getPomFileOfBundle(bundlePath));
+        results.add(buildArtifact(pom));
+        for (String moduleRelativePath : pom.getModules()) {
+            String moduleAbsPath = StringUtils.joinWith(File.separator, bundlePath,
+                moduleRelativePath);
+            results.addAll(getBundlesArtifact(moduleAbsPath));
+        }
+        return results;
+    }
+
+    private static ArtifactItem buildArtifact(Model model) {
+        String groupId = model.getGroupId() == null ? model.getParent().getGroupId()
+            : model.getGroupId();
+        String version = model.getVersion() == null ? model.getParent().getVersion()
+            : model.getVersion();
+        String type = model.getPackaging() == null ? "jar" : model.getPackaging();
+        return ArtifactItem.builder().groupId(groupId).artifactId(model.getArtifactId())
+            .version(version).type(type).build();
+    }
+
+    public static File getPomFileOfBundle(String bundlePath) {
+        return new File(bundlePath, "pom.xml");
+    }
+
+    public static File getPomFileOfBundle(File bundle) {
+        return new File(bundle, "pom.xml");
+    }
+
+    public static Model buildPomModel(String filePath) {
+        return buildPomModel(new File(filePath));
+    }
+
+    public static Model buildPomModel(File file) {
+        if (!file.exists()) {
+            throw new RuntimeException("ERROR, MavenUtils:buildPomModel 文件不存在" + file.getPath());
+        }
+
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            return reader.read(inputStream);
+        } catch (IOException | XmlPullParserException e) {
+            throw new RuntimeException(
+                "ERROR, MavenPomUtil:buildPomModel for" + file.getPath() + "\nException:" + e);
+        }
+    }
+
+    public static Model buildPomModel(InputStream inputStream) {
+        try {
+            return reader.read(inputStream);
+        } catch (IOException | XmlPullParserException e) {
+            throw new RuntimeException("ERROR, MavenPomUtil:buildPomModel" + e);
+        }
+    }
+
+    public static void writePomModel(File file, Model model) {
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            writer.write(fileWriter, model);
+        } catch (IOException e) {
+            throw new RuntimeException(
+                "ERROR, MavenPomUtil:buildPomModel for" + file.getPath() + "\nException:" + e);
+        }
+    }
+
+    public static InvocationResult invoke(MavenSession mavenSession, String goal,
+                                          File pomFile) throws MavenInvocationException,
+                                                        MojoExecutionException {
+
+        InvocationRequest request = new DefaultInvocationRequest();
+        request.setPomFile(pomFile);
+        List<String> goals = new ArrayList<>(Collections.singletonList(goal));
+        Properties userProperties = mavenSession.getCurrentProject().getProjectBuildingRequest()
+            .getUserProperties();
+        if (userProperties != null) {
+            userProperties.forEach((key, value) -> goals.add(String.format("-D%s=%s", key, value)));
+        }
+        request.setGoals(goals);
+        request.setBatchMode(mavenSession.getSettings().getInteractiveMode());
+        request.setProfiles(mavenSession.getSettings().getActiveProfiles());
+        setSettingsLocation(mavenSession, request);
+        Invoker invoker = new DefaultInvoker();
+        return invoker.execute(request);
+
+    }
+
+    private static void setSettingsLocation(MavenSession mavenSession, InvocationRequest request) {
+        File userSettingsFile = mavenSession.getRequest().getUserSettingsFile();
+        if (userSettingsFile != null && userSettingsFile.exists()) {
+            request.setUserSettingsFile(userSettingsFile);
+        }
+        File globalSettingsFile = mavenSession.getRequest().getGlobalSettingsFile();
+        if (globalSettingsFile != null && globalSettingsFile.exists()) {
+            request.setGlobalSettingsFile(globalSettingsFile);
+        }
+    }
+
+    public static Set<Artifact> getDependencyArtifacts(MavenProject mavenProject) throws MojoExecutionException {
+        DependencyListMojo mojo = new DependencyListMojo(mavenProject);
+        DependencyStatusSets statusSets = mojo.getDependencySets();
+        return statusSets.getResolvedDependencies();
+    }
+
+    private static class DependencyListMojo extends ListMojo {
+        MavenProject mavenProject;
+
+        DependencyListMojo(MavenProject project) {
+            mavenProject = project;
+        }
+
+        public DependencyStatusSets getDependencySets() throws MojoExecutionException {
+            return super.getDependencySets(false);
+        }
+
+        @Override
+        public MavenProject getProject() {
+            return this.mavenProject;
+        }
+    }
+
+}
