d340b96d37eab1a241213d64759b5623cf5f7a08
langke <langke93@163.com>
2024-08-13 16:50:23 +0800

    Update BatchInstallHelper.java
    优先级排序使用HashMap，因为HashMap是无序，会造成优先级无法按预期顺序部署,改为LinkedHashMap

--- arklet-core/src/main/java/com/alipay/sofa/koupleless/arklet/core/ops/BatchInstallHelper.java
+++ arklet-core/src/main/java/com/alipay/sofa/koupleless/arklet/core/ops/BatchInstallHelper.java
@@ -26,6 +26,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.jar.JarFile;
